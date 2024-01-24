0bc8405cf6d3786646199ac439f779eef6666bf4
Subham67i <gitsubham67i@gmail.com>
2024-01-24 21:34:43 +0530

    add-commit

---
+++ 01-Js-types-of-operators/app.js
@@ -0,0 +1,86 @@
+
+
+// javascript types of Operators  -------------------
+ 
+// const a = 3
+// const b = 4;
+// const z = a + b;
+// console.log(z);
+
+// const a = 3
+// const b = 4;
+// console.log( a + b);
+
+// let a = 30;
+// let b = 15;
+// let c = a % b ;
+// console.log(c);
+
+
+// let x = 15
+//  x++;  //  x = x + 1;
+
+//  console.log(x);
+
+// let y = 15 
+// y--;    // y = y - 1;
+// console.log(y);
+
+// Assignment Operators --------------------------
+// https://codepen.io/ecemgo/pen/YzBZjjb
+// let firstNumber = 20
+// firstNumber += 5;
+// console.log(firstNumber);
+
+
+// let secondNumber = 25;
+// secondNumber -= 5;
+// console.log(secondNumber);
+
+// Comparison Operators ------------------------------
+
+let a = 23;
+let b = 35; // '35'
+const c = a==b;
+console.log(c);  // fals
+
+let x = 23;
+let y = 35;
+const z = x != y;
+console.log(z);   // true
+
+// let firstNumber = 23;
+// let secondNumber = "23";
+// const result = firstNumber === secondNumber;
+// console.log(result); // false
+
+// let firstNumber = 23;
+// let secondNumber = 25;
+// const result = firstNumber < secondNumber;
+// console.log(result);
+
+// logical Operators -------------------------------
+
+// let firstNumber = true;
+// let secondNumber = false;
+// const result = firstNumber && secondNumber;
+// console.log(result);     // false
+
+// let firstNumber = true;
+// let secondNumber = false;
+// const result = firstNumber || secondNumber;
+// console.log(result);   // true
+
+let firstNumber = true;
+let secondNumber = false;
+const result = firstNumber || secondNumber;
+console.log(!firstNumber);   // false
+
+
+
+
+
+
+
+
+
