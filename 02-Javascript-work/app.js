834332dd7294812ca51f8db8ecba06b78b5e98f7
Subham67i <gitsubham67i@gmail.com>
2024-01-25 20:45:03 +0530

    add

---
+++ 02-Javascript-work/app.js
@@ -0,0 +1,23 @@
+// let a = 25;
+// a = a + 1;
+// console.log(a);
+
+// a = '23'
+// a = a + 1;
+// console.log(a);
+// console.log('the value of a is ', a);
+// console.log('the type of a is', typeof(a));
+
+// javascript conversion --------------------
+
+// let a = 35;
+//  a = String(a)
+
+//  a = '35'
+//  a = Number(a)
+
+//  a = 2 
+//  a = Boolean(a)
+// console.log('the value of a is ', a);
+// console.log('the type of a is', typeof(a));
+
