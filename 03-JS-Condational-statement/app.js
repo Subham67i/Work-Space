ff6f83d80e43ef47395970dcfab7e5b0f732d6bd
Subham67i <gitsubham67i@gmail.com>
2024-01-27 19:08:31 +0530

    add commit

---
+++ 03-JS-Condational-statement/app.js
@@ -0,0 +1,55 @@
+// ---------------- Conditional Statements -------------
+
+// let age = 17 ;
+
+// if(age >= 18){
+//     console.log('you are can vote');
+// }else{
+//     console.log('you acnnot vote');
+// }
+
+// console.log('this will execute always');
+
+
+// if-else-else-if-else --  
+
+// let marks = 75;
+
+// if(marks >= 90){
+//     console.log('A+');
+// }else if(marks >= 80){
+//     console.log('A');
+// }else if(marks > 70){
+//     console.log('B+');
+// }else if(marks > 60){
+//     console.log('B');
+// }else{
+//     console.log('Low marks');
+// }
+
+// nested if-else statement --------------
+
+// let age = 17 ;
+ 
+// if(age >= 18){
+//     console.log('you are can vote');
+
+//     if(age > 60){
+//         console.log('you cannot vote');
+//     }
+// }else{
+//     console.log('you acnnot vote');
+//  if(age < 5){
+//     console.log('you go to school');
+//  }
+// }
+
+// console.log('this will execute always');
+
+// tarnary Operators ---- (?)---
+
+let marks1 = 60;
+
+let result = marks1 >= 40 ? "passed" : "failed";
+console.log("result",result);
+
