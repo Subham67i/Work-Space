f6b867f651e77f787dcfa8a5134448422903f0e0
Subham67i <gitsubham67i@gmail.com>
2024-01-28 20:35:22 +0530

    commit

---
+++ 04-JS-switch-statement/app.js
@@ -0,0 +1,23 @@
+// switch statement -----
+
+let grade = "A";
+
+switch(grade){
+    case "A" : 
+    console.log('A - very good');
+    break;
+
+    case "B" : 
+    console.log('B - keep learning');
+    break;
+
+    case "C" : 
+    console.log('C - need improve');
+    break;
+
+    case "D" : 
+    console.log('D - not good');
+    break;
+    default :
+    console.log('invalid');
+}
